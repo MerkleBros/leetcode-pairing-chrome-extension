aa7c8d5c35a80c21db2198aa40bb6b2fd511b337
DouglasDev <doug012310@yahoo.com>
2018-12-14 20:04:47 -0500

    add source directory, add app and statusbar components

---
+++ chrome-extension/background.js
@@ -0,0 +1,1 @@
+console.log("you are at leet code")
---
+++ source/client-app.js
@@ -0,0 +1,48 @@
+const data = {
+  problem:{title:"Easy Problem",
+           difficulty:"easy",
+           number: 666,
+           description: 'Determine if NP=P'
+          },
+  partner: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64'
+  }
+};
+
+class StatusBar extends React.Component{
+  constructor(props){
+    super(props)
+    this.partner=props.partner
+    this.state={partnerConnected:false, partnerAction:"idle" }
+  }
+  render(){
+     return (
+        <div id={this.props.id}>
+            <img src={this.partner.avatarUrl} ></img>
+            You are pairing with: {this.partner.name} <span>&nbsp;</span>
+            Partner Status:   {this.state.partnerConnected ? "connected" : "disconnected"} <span>&nbsp;</span>
+            Partner is:{this.state.partnerAction}
+        </div>)
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.properties=props.datas
+  }
+  render(){
+    return (<React.Fragment>
+      <StatusBar  id="bar" partner={this.properties.partner} />
+      </React.Fragment>)
+  }
+}
+ 
+ReactDOM.render(
+  <App datas={data} />,
+  document.getElementById('root')
+);
+     // <Problem problem={this.props.problem} />
+    //      <Code />
+     //     <Notes />
