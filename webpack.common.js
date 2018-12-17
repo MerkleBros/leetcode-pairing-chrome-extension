28db3f0d0843d23286887772865dd27d1fc7c32a
patricklaptop <patrick@merklebros.com>
2018-12-16 22:59:20 -0600

    Add chunk optimization to prevent duplicate dependency loads
    Building bundles will now make a separate bundle for vendor libraries and other shared dependencies so that they are not bundled again for each entry point.

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,11 @@ module.exports = {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		}
+	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebPackPlugin({
