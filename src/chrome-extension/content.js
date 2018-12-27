df5ed92998720598b42c3cdccf1647587f7ba9ac
DouglasDev <doug012310@yahoo.com>
2018-12-27 17:05:38 -0500

    icon greyed out if not on leetcode

--- src/chrome-extension/content.js
+++ src/chrome-extension/content.js
@@ -1,7 +1,35 @@
-console.log('content.js loaded')
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log("content.js got message "+ JSON.stringify(request));
-  sendResponse();
-})
+const data = {
+  problem:{title:"Easy Problem",
+           difficulty:"easy",
+           number: 666,
+           description: 'Determine if NP=P'
+          },
+  partner: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64'
+  }
+};
+
+console.log("content.js loaded")
+
+// //works
+// setInterval(function(){
+//  chrome.runtime.sendMessage({data: "Hello app, how are you"});
+// },500)
 
 
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   console.log('app got message from content')
+//     sendResponse({
+//         data: "I am fine, thank you. How is life in the background?"
+//     }); 
+// });
+// chrome.extension.onRequest.addListener(
+//   function(request, sender, sendResponse) {
+//   	console.log("got a request")
+//   	if (request.senderPage=="app" && request.message=="giveMeProblemData"){
+//   		console.log("got request from app for problem data")
+//   		chrome.extension.sendRequest({senderPage:"leetCode",message:"problemData",data:data})
+//   		console.log("sent problem data to app")
+//   	}
+// });
--- src/chrome-extension/popup.js
+++ src/chrome-extension/popup.js
@@ -1,36 +1,18 @@
-
 document.getElementById("start-pairing").addEventListener('click', () => startApp());
-
-let mockData = {
-  problem:{title:"Easy Problem",
-           difficulty:"easy",
-           number: 666,
-           description: 'Determine if NP=P'
-          },
-  partner: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64'
-  }
-};
-// 	console.log('start pairing')
-
-// 	startApp()
 
 // 	chrome.runtime.sendMessage({foo:"hi I'm popup.js. What's your name?"}, 
 // 	 	function(response) {
 // 	// 	//	console.log(response.farewell);
 // 	});
 
-// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//   		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-//     		console.log('got it!');
-//   		});
-// 	});
-// })
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//  		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+//    		console.log('got it!');
+//  		});
+// });
 
 async function startApp() {
+
 	await chrome.tabs.create({"url": "index.html"});
 	//getLeetcodeData()
-	//
 }
-
